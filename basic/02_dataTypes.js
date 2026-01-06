0ede16b855c6bc8377585485c968c29ad5e2f699
Mohd Ahmad <advancetechlko32@gmail.com>
2026-01-06 08:49:40 +0000

    Add dataType

---
+++ basic/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as as newer version
+
+// alert(2+2)  we are using nodejs, not browser
+
+console.log(4 
+    +
+     4);  // code readability should be high
+console.log("hi")
+
+let name="ahmad"
+let age=44  
+let isLoggedIn = false
+
+// number => 2 to 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined =>
+// symbol => unique 
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
